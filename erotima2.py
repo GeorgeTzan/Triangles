289ade69a53cd9c5d3e819fecf8801d93b01db64
George Tzanopoulos <giwrgos.tzanopoulos@gmail.com>
2024-03-07 21:57:44 +0200

    -Removed __name__ checking

--- erotima2.py
+++ erotima2.py
@@ -2,36 +2,34 @@ import itertools
 import unittest
 from utils import *
 
+points = []
+with open("points.txt", "r") as file:
+    for line in file:
+        x, y = map(int, line.strip().split())
+        points.append((x, y))
 
-def main():
-    points = []
-    with open("points.txt", "r") as file:
-        for line in file:
-            x, y = map(int, line.strip().split())
-            points.append((x, y))
+triangles = []
 
-    triangles = []
+for triangle in itertools.combinations(points, 3):
+    triangles.append(triangle)
 
-    for triangle in itertools.combinations(points, 3):
-        triangles.append(triangle)
+valid_triangles = []
+triangle_areas = []
+for triangle in triangles:
+    area = triangle_area(triangle[0], triangle[1], triangle[2])
+    if area > 1e-6:
+        triangle_areas.append(area)
+        valid_triangles.append(triangle)
 
-    valid_triangles = []
-    triangle_areas = []
-    for triangle in triangles:
-        area = triangle_area(triangle[0], triangle[1], triangle[2])
-        if area > 1e-6:
-            triangle_areas.append(area)
-            valid_triangles.append(triangle)
+print(f"Valid Triangles: {len(valid_triangles)}")
+meanFunct = mean(triangle_areas)
+medianFunct = median(triangle_areas)
+stdevFunct = stdev(triangle_areas)
 
-    print(f"Valid Triangles: {len(valid_triangles)}")
-    meanFunct = mean(triangle_areas)
-    medianFunct = median(triangle_areas)
-    stdevFunct = stdev(triangle_areas)
-
-    print("Results using custom functions:")
-    print(
-        f"Mean area: {meanFunct:.2f}\nMedian area: {medianFunct:.2f}\nSt deviation areas: {stdevFunct:.2f}"
-    )
+print("Results using custom functions:")
+print(
+    f"Mean area: {meanFunct:.2f}\nMedian area: {medianFunct:.2f}\nSt deviation areas: {stdevFunct:.2f}"
+)
 
 
 class TestTriangleAreas(unittest.TestCase):
@@ -42,6 +40,4 @@ class TestTriangleAreas(unittest.TestCase):
         self.assertAlmostEqual(stdevFunct, 2843.23, places=2)
 
 
-if __name__ == "__main__":
-    main()
-    unittest.main()
+unittest.main()
